0fa4151b29ab434827f3e0325ce76f2af6e18bcd
Aaron Donaldson <56371059+atreids@users.noreply.github.com>
2022-06-14 22:11:48 +0100

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -7,9 +7,9 @@
 #Or die ("failed to connect to databse");
 
 $serveraddress = "localhost";
-$username = "HNDCSSA6";
-$dbpassword = "HpDPLzWDuw";
-$dbname = "HNDCSSA6";
+$username = "####";
+$dbpassword = "#####;
+$dbname = "####";
 
 $db = mysqli_connect($serveraddress, $username, $dbpassword, $dbname)
 Or die ("failed to connect to databse");
